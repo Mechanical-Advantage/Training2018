c58140fb1d7a94506cda00a60780834223d0f07e
Vishal Yelisetti <VYelisetti@zollmed.com>
2018-11-28 16:07:44 -0500

    "Committing method"

--- examples/java/week5/MinofTwoNumbers.java
+++ examples/java/week5/MinofTwoNumbers.java
@@ -18,7 +18,19 @@ class MinofTwoNumber {
         return min_num;
     }
 
+    public static double min_two_num(double a, double b) {
+        double min_num;
+        if (a < b) {
+            min_num = a;
+        } else {
+            min_num = b;
+        }
+        System.out.println(min_num);
+        return min_num;
+    }
+
     public static void main(String[] args) {
         min_two_num(2, 3);
+        min_two_num(3.1, 1.0);
     }
 }
