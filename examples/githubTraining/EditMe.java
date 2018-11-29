a3c6357efcead70b6344c5440ffe9e3ff66ff7e1
Luke230 <lukeswartz230@gmail.com>
2018-11-28 20:20:11 -0500

    Adding Luke's Code

--- examples/githubTraining/EditMe.java
+++ examples/githubTraining/EditMe.java
@@ -4,5 +4,6 @@ class EditMe {
         // Ensure that you add your imports to this file so that it knows the class it is pick up from
         // Add your method call here
         Vishal.printSomeStatement();
+        luke2.luke3();
     }
 }
